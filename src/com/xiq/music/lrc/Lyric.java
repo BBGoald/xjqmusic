905f492fb6cc2b840090e4342559cceebb340af9
BBGoald <15077307158@163.com>
2015-03-30 15:38:27 +0800

    modify Lyric.java

--- src/com/xiq/music/lrc/Lyric.java
+++ src/com/xiq/music/lrc/Lyric.java
@@ -10,12 +10,22 @@ public class Lyric {
 		if (textContent == null) {
 			return "";
 		}
-		return null;
+		
+		String textString = textContent.replace("<br />", "\n");
+		textString = textString.replace("\\<.*?\\>", "");
+		return textString;
 	}
 
 	public void addTextContent(Lyric lyric) {
 		if (this.startTime.mSeconds <= lyric.startTime.mSeconds) {
-			
+			textContent = textContent + "<br />" + lyric.textContent;//当前歌词 换行 添加下一行歌词
+		}else {
+			textContent = lyric.textContent + "<br />" + textContent;//当前歌词超前于添加的歌词
+			this.startTime.mSeconds = lyric.startTime.mSeconds;
+		}
+		
+		if (this.endTime.mSeconds < lyric.endTime.mSeconds) {
+			this.endTime.mSeconds = lyric.endTime.mSeconds;
 		}
 	}
 	
