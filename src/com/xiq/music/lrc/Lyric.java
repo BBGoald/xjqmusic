fa95b24ffbdbaa8cf381d37a1a8a9c844bf5841a
BBGoald <15077307158@163.com>
2015-04-07 15:05:58 +0800

    improve FormatLyric.java Lyric.java Time.java

--- src/com/xiq/music/lrc/Lyric.java
+++ src/com/xiq/music/lrc/Lyric.java
@@ -4,23 +4,23 @@ public class Lyric {
 	
 	public Time startTime;//在TimeTextObject中用到
 	public Time endTime;
-	private String textContent;
+	public String contentString;
 	
 	public String getTextContent() {
-		if (textContent == null) {
+		if (contentString == null) {
 			return "";
 		}
 		
-		String textString = textContent.replace("<br />", "\n");
+		String textString = contentString.replace("<br />", "\n");
 		textString = textString.replace("\\<.*?\\>", "");
 		return textString;
 	}
 
 	public void addTextContent(Lyric lyric) {
 		if (this.startTime.mSeconds <= lyric.startTime.mSeconds) {
-			textContent = textContent + "<br />" + lyric.textContent;//当前歌词 换行 添加下一行歌词
+			contentString = contentString + "<br />" + lyric.contentString;//当前歌词 换行 添加下一行歌词
 		}else {
-			textContent = lyric.textContent + "<br />" + textContent;//当前歌词超前于添加的歌词
+			contentString = lyric.contentString + "<br />" + contentString;//当前歌词超前于添加的歌词
 			this.startTime.mSeconds = lyric.startTime.mSeconds;
 		}
 		
--- src/com/xiq/music/lrc/Time.java
+++ src/com/xiq/music/lrc/Time.java
@@ -1,10 +1,27 @@
 package com.xiq.music.lrc;
 
+import android.R.integer;
+
 public class Time {
 
 	protected int mSeconds;
 
 	public Time(int ms){
 		this.mSeconds = ms;
+	}
+	protected Time(String format, String value) {
+		try {
+			if (format.equalsIgnoreCase("mm:ss.cs")) {
+				int m, s, cs;
+				m = Integer.parseInt(value.substring(0, 2));
+				s = Integer.parseInt(value.substring(3, 8));
+				cs = Integer.parseInt(value.substring(6, 8));
+				mSeconds = cs*10 + s*1000 + m*60000;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			mSeconds = 0;
+		}
 	}
 }
